eaf10636669c29e84760af124193929dd3ffc0c9
Sanctuary <sanc.pw@gmail.com>
2020-01-04 00:25:02 -0500

    Initial commit

---
+++ src/components/discord-messages/discord-messages.tsx
@@ -0,0 +1,30 @@
+import { Component, Prop, h, Host } from '@stencil/core'
+
+@Component({
+	tag: 'discord-messages',
+	styleUrl: 'discord-messages.css',
+	shadow: true,
+})
+export class DiscordMessages {
+	/**
+	 * Whether to use light theme or not.
+	 */
+	@Prop() lightTheme: boolean = false
+
+	/**
+	 * Whether to use compact mode or not.
+	 */
+	@Prop() compactMode: boolean = false
+
+	render() {
+		const layoutClasses: string[] = [];
+		if (this.lightTheme) layoutClasses.push('discord-light-theme')
+		if (this.compactMode) layoutClasses.push('discord-compact-mode')
+
+		return (
+			<Host class={`discord-messages ${layoutClasses.join(' ')}`}>
+				<slot></slot>
+			</Host>
+		)
+	}
+}
